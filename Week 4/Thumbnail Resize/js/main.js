a6d634f79e6d7c1bc60a34196a1b02374fd4eb25
Crystal <33204245+cgrave@users.noreply.github.com>
2018-01-29 09:51:12 -0700

    Update main.js
    not a solution

--- Week 4/Thumbnail Resize/js/main.js
+++ Week 4/Thumbnail Resize/js/main.js
@@ -2,3 +2,20 @@
 // should be added to the div.thumbnails element.  This will apply the corresponding
 // CSS rule to  modify the display the of hte thumbnail images.  In addition to this,
 // the clicked button should be made active (i.e. add the active class).
+
+
+//select the links
+//change active to the one clicked on
+//when that active is clicked on then change the image sizes
+
+var activeLink = document.querySelector('ul.thumbnail-sizes');
+function handleFeatureClick() {
+	var imgActive = document.querySelector("ul.thumbnail-sizes");
+	if(imgActive.classList.contains("active")){
+		console.log("hello");
+	}
+	
+//thumbnail-size.classList.contains("active");
+}
+//event listener
+activeLink.addEventListener('click', handleFeatureClick);
