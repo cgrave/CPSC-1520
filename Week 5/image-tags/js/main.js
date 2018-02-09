1ba0fe162377e2d6986f7b1e49e2e455cef133b5
Crystal <33204245+cgrave@users.noreply.github.com>
2018-02-09 09:01:18 -0700

    Update main.js

--- Week 5/image-tags/js/main.js
+++ Week 5/image-tags/js/main.js
@@ -1,27 +1,25 @@
-// When the 'Add Tag' submit button is clicked, the p.feature.tags element should be 
-document.querySelector('.feature.frm').addEventListener('submit', function (evt) {
-	//var inputTag = evt.target.elements.tags.value;
-	var frm = evt.target;
-	var tag = frm.elements.tags;
+document.querySelector('.feature.frm ').addEventListener( 'submit', function (evt) {
+var frm = evt.target;
+var tag = frm.elements.tags;
+var error = document.querySelector('p.feature.error');
 
-	document.querySelector('p.feature.tags ').innerHTML += '#' + tag.value;
-	tag.value = '';
-	evt.preventDefault;
-})
-// document.querySelector('.feature.frm ').addEventListener( 'submit', function (evt) {
-// 	 var frm = evt.target;
-// 	 var tag = frm.elements.tags;
+	if(tag.value.trim() == ''){
+		error.classList.remove('hidden');
+	 	error.innerHTML = '<p>Tags cannot be empty</p>';
+	}
+	else if(tag.value.split(" ").length >= 2){
+		error.classList.remove('hidden');
+		error.innerHTML = '<p>Tags cannot contain spaces</p>';
+	}
+	else{
+		document.querySelector('p.feature.tags ').innerHTML += ' #' + tag.value;
+		tag.value = '';
+		error.classList.add('hidden');
+		error.innerHTML = '';
+	}
 
-// 	 document.querySelector('p.feature.tags ').innerHTML += '#' + tag.value;
-// 	 tag.value = '';
-// 	 evt.preventDefault();
-// });
+	 evt.preventDefault();
+});
 
-// updated with the tag that was entered by the user.  Do not overwrite any existing tags,
-
-// simply add each additional tag onto the already existing list.  A tag cannot be empty 
-// (i.e. cannot be whitespace). If the user tries to add an empty tag, remove the hidden 
-// class from the p.feature.error element.  If the form is submitted correctly, then ensure
-// that the p.feature.error element is hidden.
 
 
