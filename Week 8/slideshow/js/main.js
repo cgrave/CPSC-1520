4373ca91a55c67cb8ed9f94b8359c8e04ceb0b9a
Crystal <33204245+cgrave@users.noreply.github.com>
2018-03-05 09:32:12 -0700

    Update main.js

--- Week 8/slideshow/js/main.js
+++ Week 8/slideshow/js/main.js
@@ -1,14 +1,25 @@
 // 
 var images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
+var currentImg = 0;
 //document.querySelector('.carousel>img').src = 'images/' + images[0];
 document.querySelector('.carousel').addEventListener('click', function(evt){
 	var target = evt.target;
 	if(target.classList.contains('control')) {
-		console.log('control clicked...');
+		if(target.classList.contains('next')){
+			//move to the next index in the array
+			currentImg +=1;
+		} else if(target.classList.contains('prev')){
+			//move to the prev index in array
+			currentImg -=1;
+		} else{
+			
+		}
+		//display the new current image
+		document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 	}
 });
 
 
-// 
+// how do i make it so it just goes around not continue addding numbers?
 
 // 
