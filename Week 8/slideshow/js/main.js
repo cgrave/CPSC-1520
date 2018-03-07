b6b0580ae284350380e024d0668c28e6f7cc33f8
Crystal <33204245+cgrave@users.noreply.github.com>
2018-03-07 08:15:34 -0700

    Update main.js

--- Week 8/slideshow/js/main.js
+++ Week 8/slideshow/js/main.js
@@ -1,4 +1,4 @@
-// 
+/*// 
 var images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
 var currentImg = 0;
 //document.querySelector('.carousel>img').src = 'images/' + images[0];
@@ -17,33 +17,33 @@ document.querySelector('.carousel').addEventListener('click', function(evt){
 		//display the new current image
 		document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 	}
-});
+});*/
 
 
 // how do i make it so it just goes around not continue addding numbers?
 
 // this kinda works
-/*var images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
+var images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
 var currentImg = 0;
 document.querySelector('.carousel').addEventListener('click', function(evt){
 	var target = evt.target;
 	if(target.classList.contains('control')) {
 		if(target.classList.contains('next')){
 			//move to the next index in the array
-			if(currentImg >= (images.length-1)){
+			if(currentImg >= (images.length - 1)){
 				currentImg = 0;
 				console.log('setting to zero from next button');
 			} else{
-				currentImg +=1;
+				currentImg += 1;
 				console.log('increment current img from next button');
 			}
 		} else if(target.classList.contains('prev')){
 			//move to the prev index in array
 			if(currentImg <= 0){
-				currentImg = 0;
+				currentImg = images.length - 1;
 				console.log('setting current img to zero from prev button');
 			} else{
-				currentImg -=1;
+				currentImg -= 1;
 				console.log('decrement current img from prev button');
 			}
 			
@@ -51,4 +51,5 @@ document.querySelector('.carousel').addEventListener('click', function(evt){
 		//display the new current image
 		document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 	}
-});*/
+});
+document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
